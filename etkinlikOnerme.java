f12564f3719f464fd0b51ef3da0ee9a5fc73bc47
Yusuf Bakır <yusufbkr23@gmail.com>
2023-07-25 15:39:09 +0300

    Hava Sıcaklığına Göre Etkinlik Önerme Programı

---
+++ etkinlikOnerme.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class etkinlikOnerme {
+    public static void main(String[] args) {
+        
+        Scanner inp = new Scanner(System.in);
+        int heat;
+
+        System.out.print("Lütfen Hava sıcaklığını Belirtiniz : ");
+        heat = inp.nextInt();
+
+        if (heat<5){
+            System.out.print("Kayak yapabilirsiniz");
+        }
+        else if (heat>=5 && heat<10) {
+            System.out.print("Sinemaya gidebilirsiniz");
+        }
+        else if(heat>=10 && heat<=15){
+            System.out.print("Pikniğe gidebilirsiniz");
+        }
+        else if (heat>15 && heat<=25){
+            System.out.print("Sahile gidebilirsiniz");
+        }
+        else {
+            System.out.print("Yüzmeye gidebilirsiniz");
+        }
+    }
+}
