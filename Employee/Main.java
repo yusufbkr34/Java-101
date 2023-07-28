e14e96adc64992f1715102779404bde34bf9282b
Yusuf Bakır <yusufbkr23@gmail.com>
2023-07-28 21:01:47 +0300

    Employee

---
+++ Employee/Main.java
@@ -0,0 +1,15 @@
+package Employee;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Roman",2000,45,1995);
+        Employee e2 = new Employee("Steven",2000,50,2000);
+        e1.printInfo();
+        e2.printInfo();
+
+
+
+
+    }
+}
