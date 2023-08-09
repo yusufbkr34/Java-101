8e97cadced847806e746787e24c91eef6776004c
Yusuf Bakır <yusufbkr23@gmail.com>
2023-08-09 17:22:15 +0300

    Multipleof4N5

---
+++ Multipleof4N5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Multipleof4N5 {
+    public static void main(String[] args) {
+     
+        
+        
+        int n;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n = scan.nextInt();
+
+        System.out.println("Multiple of 4 : ");
+        for (int i = 1; i <= n; i *= 4) {
+            System.out.println(i);
+        }
+        System.out.println("----------------");
+        System.out.println("Multiple of 5 : ");
+        for (int i = 1; i <= n; i *= 5) {
+            System.out.println(i);
+        }
+    }
+}
