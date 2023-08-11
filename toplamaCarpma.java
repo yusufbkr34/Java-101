b4f69f9b2df5126075e2994c005104ea62894457
Yusuf Bakır <yusufbkr23@gmail.com>
2023-08-11 14:33:30 +0300

    ToplamaCarpma

---
+++ toplamaCarpma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class toplamaCarpma {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int toplam = 0;
+        
+        System.out.print("Bir sayi giriniz: ");
+        int sayi1 = scan.nextInt();
+        System.out.print("Bir sayi giriniz:");
+        int sayi2 = scan.nextInt();
+        
+        for(int i = 0; i < sayi2; i++)
+        {
+            toplam += sayi1;
+        }
+        System.out.println(sayi1 + "X" + sayi2 + "= " + toplam);
+    }
+}
