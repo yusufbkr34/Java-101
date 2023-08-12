f5bffa4573424abdb5787ec2f882508fdc1eb23c
Yusuf Bakır <yusufbkr23@gmail.com>
2023-08-12 14:29:26 +0300

    Üretilen random sayının rakamlar toplamı

--- randomSayiToplami.java
+++ randomSayiToplami.java
@@ -1,15 +1,15 @@
 public class randomSayiToplami {
     public static void main(String args[])
     {
-        int randomSayi = (int)(Math.random()*(1000)); 
-        System.out.println("Random sayı : " + randomSayi); 
+        int randomSayi = (int)(Math.random()*(1000)); //0 ile 1000 arasında random sayı ürettik.
+        System.out.println("Random sayı : " + randomSayi); //üretilen sayıyı konsola yazdırdık.
 
-        int birler = randomSayi %10; 
+        int birler = randomSayi %10; //birler basamağı
 
         randomSayi = randomSayi /10; 
-        int onlar = randomSayi %10;   
+        int onlar = randomSayi %10;   //onlar basamağı
         
-        int yuzler = randomSayi /10;  
+        int yuzler = randomSayi /10;  //yüzler basamağı
 
         System.out.println("Toplamları ="+(birler+onlar+yuzler));
 
